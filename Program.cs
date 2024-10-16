ea7189c0be15c93365c208d2dcc72247071e21cb
João Vitor <148113167+joao-vitor8217@users.noreply.github.com>
2024-10-16 09:56:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+class Progam {
+    public static void Main(string[] args)
+    {
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+        double n = 3.14159;
+        double raio = double.Parse(Console.ReadLine());
+
+        double calculo = n * Math.Pow(raio, 2);
+
+        Console.WriteLine("X=" + calculo);
+
+
+
+
+        
+    }
+}
